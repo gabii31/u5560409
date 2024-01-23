16e10ec41203bf008c54a7cad675a6ace05f4287
gabii31 <156658607+gabii31@users.noreply.github.com>
2024-01-23 15:38:15 +0000

    Update import random.py

--- import random.py
+++ import random.py
@@ -2,15 +2,18 @@ import random
 n = random.randint(1,20) #n is the number
 print('i am thinking of a number between 1 and 20')
 
-print('take a guess')
-i = int(input())
+for times in range(1,7):
+      print('take a guess')
+      i = int(input())
 
-if i>n:
-      print('too high')
-elif i<n:
-     print('too low')
+      if i>n:
+            print('too high')
+      elif i<n:
+            print('too low')
+      else:
+            break
 
-       
-
-if i == n:
-   print('goodjob!')
+      if i == n:
+            print('goodjob!')
+      else:
+            print("the number i was thinking of was str(n))
