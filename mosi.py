bb745d0a927c8602aa48ef756b8373aa2cb4342e
gabii31 <156658607+gabii31@users.noreply.github.com>
2024-01-29 12:13:42 +0000

    Add files via upload

---
+++ mosi.py
@@ -0,0 +1,46 @@
+def morse_translator(text):
+    # Morse code mapping
+    morse_code_dict = {
+        "A": ".-",
+        "B": "-...",
+        "C": "-.-.",
+        "D": "-..",
+        "E": ".",
+        "F": "..-.",
+        "G": "--.",
+        "H": "....",
+        "I": "..",
+        "J": ".---",
+        "K": "-.-",
+        "L": ".-..",
+        "M": "--",
+        "N": "-.",
+        "O": "---",
+        "P": ".--.",
+        "Q": "--.-",
+        "R": ".-.",
+        "S": "...",
+        "T": "-",
+        "U": "..-",
+        "V": "...-",
+        "W": ".--",
+        "X": "-..-",
+        "Y": "-.--",
+        "Z": "--..",
+    }
+    codes = ""
+    for char in text.upper():
+        if char != " ":
+            codes += morse_code_dict[char] + " "
+        else:
+            codes += "/ "
+
+    return codes.strip()
+
+print(
+    morse_translator("HELLO WORLD")
+)
+print(morse_translator("Python"))
+print(
+    morse_translator("Morse Code")
+)
